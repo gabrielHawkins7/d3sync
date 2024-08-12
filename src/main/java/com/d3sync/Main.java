1ea139b643fa9796e1c5900b341b547b022b3dd8
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-08-12 15:10:57 -0400

    WOOOOOO

--- src/main/java/com/d3sync/Main.java
+++ src/main/java/com/d3sync/Main.java
@@ -3,6 +3,7 @@ package com.d3sync;
 import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,55 +34,75 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world");
 
-        syncDirectory("/Users/gabrielhawkins/Documents","F:\\COLDSTORE\\Laptop Backup\\Documents", new SSHConfig("HOMESERVER", "Gabe", "0258"));
+        //syncDirectory("/Users/gabrielhawkins/Documents","F:\\COLDSTORE\\Laptop Backup\\Documents", new SSHConfig("HOMESERVER", "Gabe", "0258"));
 
-        
-
+        try (SSHClient ssh = setupSshClient("HOMESERVER", "Gabe", "0258")) {
+            SFTPClient sftp = setupSftpClient(ssh);
+            File localDir = new File("/Users/gabrielhawkins/Documents");
+            String remoteDir = "F:\\COLDSTORE\\Laptop Backup\\Documents";
+            uploadDirectoryRecursively(sftp, localDir, remoteDir);
+            sftp.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-
-    static void syncDirectory(String sourceDir, String targetDir, SSHConfig config ) throws Exception{
-
+    public static SSHClient setupSshClient(String hostname, String username, String password) throws IOException {
         SSHClient ssh = new SSHClient();
-        try {
-            ssh.addHostKeyVerifier(new PromiscuousVerifier()); // Use cautiously; better to verify keys properly in production
-            ssh.connect(config.host);
-            ssh.authPassword(config.username, config.password); // or use ssh.authPublickey(username, "path_to_private_key");
+        ssh.loadKnownHosts();
+        ssh.connect(hostname);
+        ssh.authPassword(username, password);
+        return ssh;
+    }
+    
+    public static SFTPClient setupSftpClient(SSHClient ssh) throws IOException {
+        return ssh.newSFTPClient();
+    }
 
-            // Using SFTP to list files
-            try (SFTPClient sftp = ssh.newSFTPClient()) {
-                List<RemoteResourceInfo> target = sftp.ls(targetDir);
-                List<File> source = Arrays.asList(new File(sourceDir).listFiles());
 
-                for(RemoteResourceInfo t : target){
-                    if(t.isRegularFile() && )
+    public static void uploadDirectoryRecursively(SFTPClient sftp, File localDir, String remoteDir) throws IOException {
+        if (!localDir.isDirectory()) {
+            throw new IllegalArgumentException("Not a directory");
+        }
 
+        long[] stats = new long[2];
+    
+        List<RemoteResourceInfo> remoteFiles = sftp.ls(remoteDir);
+    
+        for (File localFile : localDir.listFiles()) {
+            String remoteFilePath = remoteDir + "/" + localFile.getName();
+            boolean exists = remoteFiles.stream().anyMatch(f -> f.getName().equals(localFile.getName()));
+    
+            if (localFile.isDirectory()) {
+                System.out.println("Uploading Folder " + localFile.getAbsolutePath() + " to " + remoteDir + " " + printSpeed(stats[0], stats[1]) + "mbs");
+                if (!exists) {
+                    sftp.mkdir(remoteFilePath);
                 }
-
-
-                // sftp.put(new FileSystemFile(sourceDir), targetDir);
-                // sftp.close();
-
-            }
-
-        }finally{
-            ssh.disconnect();
-        }
-
-    }
-
-    private boolean filecomp(RemoteResourceInfo a, File b){
-        return a.getName().equals(b.getName());
-    }
-    private boolean hasFile(RemoteResourceInfo a, List<File> b){
-        for(File f : b){
-            if(a.getName().equals(f.getName())){
-                return true;
+                uploadDirectoryRecursively(sftp, localFile, remoteFilePath);
+            } else {
+                if (!exists) {
+                    System.out.println("Uploading File " + localFile.getAbsolutePath() + " to " + remoteDir + " " +printSpeed(stats[0], stats[1]) + "mbs");
+                    long startTime = System.currentTimeMillis();
+                    sftp.put(localFile.getAbsolutePath(), remoteFilePath);
+                    long endTime = System.currentTimeMillis();
+                    long duration = endTime - startTime;
+                    long fileSize = localFile.length();
+                    stats[0] += fileSize; // Total data transferred
+                    stats[1] += duration; // Total time in milliseconds
+                }
             }
         }
-        return false;
     }
-
+    private static String printSpeed(Long data, long time){
+        return String.format("%.2f", (data / 1048576.0) / (time / 1000.0));
+    }
+    public static float round2(float number, int scale) {
+        int pow = 10;
+        for (int i = 1; i < scale; i++)
+            pow *= 10;
+        float tmp = number * pow;
+        return ( (float) ( (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) ) ) / pow;
+    }
 }
 
 /**
